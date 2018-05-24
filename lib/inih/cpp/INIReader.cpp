5f377d22f8907df070933592878f01f823c6842f
SeanPesce <pesce.sean@gmail.com>
2018-05-24 09:37:45 -0400

    Add INI Reader library files

---
+++ lib/inih/cpp/INIReader.cpp
@@ -0,0 +1,86 @@
+// Read an INI file into easy-to-access name/value pairs.
+
+// inih and INIReader are released under the New BSD license (see LICENSE.txt).
+// Go to the project home page for more info:
+//
+// https://github.com/benhoyt/inih
+
+#include <algorithm>
+#include <cctype>
+#include <cstdlib>
+#include <sstream>
+#include "../ini.h"
+#include "INIReader.h"
+
+using std::string;
+
+INIReader::INIReader(const string& filename)
+{
+    _error = ini_parse(filename.c_str(), ValueHandler, this);
+}
+
+int INIReader::ParseError() const
+{
+    return _error;
+}
+
+long INIReader::GetInteger(const string& section, const string& name, long default_value) const
+{
+    string valstr = Get<std::string>(section, name, "");
+    const char* value = valstr.c_str();
+    char* end;
+    // This parses "1234" (decimal) and also "0x4D2" (hex)
+    long n = strtol(value, &end, 0);
+    return end > value ? n : default_value;
+}
+
+double INIReader::GetReal(const string& section, const string& name, double default_value) const
+{
+    string valstr = Get<std::string>(section, name, "");
+    const char* value = valstr.c_str();
+    char* end;
+    double n = strtod(value, &end);
+    return end > value ? n : default_value;
+}
+
+bool INIReader::GetBoolean(const string& section, const string& name, bool default_value) const
+{
+    string valstr = Get<std::string>(section, name, "");
+    // Convert to lower case to make string comparisons case-insensitive
+    std::transform(valstr.begin(), valstr.end(), valstr.begin(), ::tolower);
+    if (valstr == "true" || valstr == "yes" || valstr == "on" || valstr == "1")
+        return true;
+    else if (valstr == "false" || valstr == "no" || valstr == "off" || valstr == "0")
+        return false;
+    else
+        return default_value;
+}
+
+uint8_t INIReader::GetVkCode(const string& section, const string& name, uint8_t default_value) const
+{
+    string valstr = Get<std::string>(section, name, "0");
+    unsigned short vk_code = default_value;
+    std::stringstream in;
+    in << std::hex << valstr;
+    in >> vk_code;
+    return static_cast<uint8_t>(vk_code);
+}
+
+string INIReader::MakeKey(const string& section, const string& name)
+{
+    string key = section + "=" + name;
+    // Convert to lower case to make section/name lookups case-insensitive
+    std::transform(key.begin(), key.end(), key.begin(), ::tolower);
+    return key;
+}
+
+int INIReader::ValueHandler(void* user, const char* section, const char* name,
+                            const char* value)
+{
+    INIReader* reader = (INIReader*)user;
+    string key = MakeKey(section, name);
+    if (reader->_values[key].size() > 0)
+        reader->_values[key] += "\n";
+    reader->_values[key] += value;
+    return 1;
+}
---
+++ lib/inih/cpp/INIReader.h
@@ -0,0 +1,219 @@
+// Read an INI file into easy-to-access name/value pairs.
+
+// inih and INIReader are released under the New BSD license (see LICENSE.txt).
+// Go to the project home page for more info:
+//
+// https://github.com/benhoyt/inih
+
+#ifndef __INIREADER_H__
+#define __INIREADER_H__
+
+#ifndef __cplusplus
+#define __cplusplus
+#endif
+
+#include <map>
+#include <string>
+
+#ifdef _WIN32
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#include <Windows.h>
+#endif
+
+// Read an INI file into easy-to-access name/value pairs. (Note that I've gone
+// for simplicity here rather than speed, but it should be pretty decent.)
+class INIReader
+{
+public:
+    // Construct INIReader and parse given filename. See ini.h for more info
+    // about the parsing.
+    INIReader(const std::string& filename);
+
+    // Return the result of ini_parse(), i.e., 0 on success, line number of
+    // first error on parse error, or -1 on file open error.
+    int ParseError() const;
+
+    // Extendable value-getter function
+    template <typename T>
+    T Get(const std::string& section, const std::string& name) const;
+    template <typename T>
+    T Get(const std::string& section, const std::string& name, const T& default_value) const;
+
+    // Get a string value from INI file, returning default_value if not found.
+    template <>
+    std::string Get<std::string>(const std::string& section, const std::string& name, const std::string& default_value) const;
+    template <>
+    std::string Get<std::string>(const std::string& section, const std::string& name) const;
+
+    // Get an integer (long) value from INI file, returning default_value if
+    // not found or not a valid integer (decimal "1234", "-1234", or hex "0x4d2").
+    long GetInteger(const std::string& section, const std::string& name, long default_value) const;
+    template <>
+    long Get<long>(const std::string& section, const std::string& name, const long& default_value) const;
+    template <>
+    long Get<long>(const std::string& section, const std::string& name) const;
+
+    template <>
+    int Get<int>(const std::string& section, const std::string& name, const int& default_value) const;
+    template <>
+    int Get<int>(const std::string& section, const std::string& name) const;
+
+    template <>
+    short Get<short>(const std::string& section, const std::string& name, const short& default_value) const;
+    template <>
+    short Get<short>(const std::string& section, const std::string& name) const;
+
+    // Get a real (floating point double) value from INI file, returning
+    // default_value if not found or not a valid floating point value
+    // according to strtod().
+    double GetReal(const std::string& section, const std::string& name, double default_value) const;
+    template <>
+    double Get<double>(const std::string& section, const std::string& name, const double& default_value) const;
+    template <>
+    double Get<double>(const std::string& section, const std::string& name) const;
+
+    template <>
+    float Get<float>(const std::string& section, const std::string& name, const float& default_value) const;
+    template <>
+    float Get<float>(const std::string& section, const std::string& name) const;
+
+    // Get a boolean value from INI file, returning default_value if not found or if
+    // not a valid true/false value. Valid true values are "true", "yes", "on", "1",
+    // and valid false values are "false", "no", "off", "0" (not case sensitive).
+    bool GetBoolean(const std::string& section, const std::string& name, bool default_value) const;
+    template <>
+    bool Get<bool>(const std::string& section, const std::string& name, const bool& default_value) const;
+    template <>
+    bool Get<bool>(const std::string& section, const std::string& name) const;
+
+    // Get a virtual-key code value from INI file, returning 0 if not found or if
+    // not a valid hex-formatted byte value. Valid values are 0 to FF (not case
+    // sensitive). More info on virtual-key codes here:
+    //   https://msdn.microsoft.com/en-us/library/windows/desktop/dd375731(v=vs.85).aspx
+    uint8_t GetVkCode(const std::string& section, const std::string& name, uint8_t default_value = 0) const;
+    template <>
+    uint8_t Get<uint8_t>(const std::string& section, const std::string& name, const uint8_t& default_value) const;
+    template <>
+    uint8_t Get<uint8_t>(const std::string& section, const std::string& name) const;
+
+private:
+    int _error;
+    std::map<std::string, std::string> _values;
+    static std::string MakeKey(const std::string& section, const std::string& name);
+    static int ValueHandler(void* user, const char* section, const char* name,
+                            const char* value);
+};
+
+
+
+
+// Extendable value-getter function
+template <typename T>
+T INIReader::Get(const std::string& section, const std::string& name, const T& default_value) const
+{
+    return default_value;
+}
+
+template <typename T>
+T INIReader::Get(const std::string& section, const std::string& name) const
+{
+    T result;
+    return result;
+}
+
+template <>
+std::string INIReader::Get<std::string>(const std::string& section, const std::string& name, const std::string& default_value) const
+{
+    std::string key = MakeKey(section, name);
+    // Use _values.find() here instead of _values.at() to support pre C++11 compilers
+    return _values.count(key) ? _values.find(key)->second : default_value;
+}
+
+
+template <>
+long INIReader::Get<long>(const std::string& section, const std::string& name, const long& default_value) const
+{
+    return this->GetInteger(section, name, default_value);
+}
+
+template <>
+long INIReader::Get<long>(const std::string& section, const std::string& name) const
+{
+    return this->GetInteger(section, name, 0L);
+}
+
+template <>
+int INIReader::Get<int>(const std::string& section, const std::string& name, const int& default_value) const
+{
+    return static_cast<int>(this->GetInteger(section, name, default_value));
+}
+
+template <>
+int INIReader::Get<int>(const std::string& section, const std::string& name) const
+{
+    return static_cast<int>(this->GetInteger(section, name, 0L));
+}
+
+template <>
+short INIReader::Get<short>(const std::string& section, const std::string& name, const short& default_value) const
+{
+    return static_cast<short>(this->GetInteger(section, name, default_value));
+}
+
+template <>
+short INIReader::Get<short>(const std::string& section, const std::string& name) const
+{
+    return static_cast<short>(this->GetInteger(section, name, 0L));
+}
+
+template <>
+double INIReader::Get<double>(const std::string& section, const std::string& name, const double& default_value) const
+{
+    return this->GetReal(section, name, default_value);
+}
+
+template <>
+double INIReader::Get<double>(const std::string& section, const std::string& name) const
+{
+    return this->GetReal(section, name, 0.0);
+}
+
+template <>
+float INIReader::Get<float>(const std::string& section, const std::string& name, const float& default_value) const
+{
+    return static_cast<float>(this->GetReal(section, name, default_value));
+}
+
+template <>
+float INIReader::Get<float>(const std::string& section, const std::string& name) const
+{
+    return static_cast<float>(this->GetReal(section, name, 0.0));
+}
+
+template <>
+bool INIReader::Get<bool>(const std::string& section, const std::string& name, const bool& default_value) const
+{
+    return this->GetBoolean(section, name, default_value);
+}
+
+template <>
+bool INIReader::Get<bool>(const std::string& section, const std::string& name) const
+{
+    return this->GetBoolean(section, name, false);
+}
+
+template <>
+uint8_t INIReader::Get<uint8_t>(const std::string& section, const std::string& name, const uint8_t& default_value) const
+{
+    return this->GetVkCode(section, name, default_value);
+}
+
+template <>
+uint8_t INIReader::Get<uint8_t>(const std::string& section, const std::string& name) const
+{
+    return this->GetVkCode(section, name, 0);
+}
+
+#endif  // __INIREADER_H__
